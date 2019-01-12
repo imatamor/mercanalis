a6c7718ab15e276d63041c5c33dd16d2238c2d0f
Isaac Matamoros <isaacmg4@hotmail.com>
2019-01-12 10:40:04 -0500

    Web Services

---
+++ services/login.php
@@ -0,0 +1,22 @@
+<?php
+
+include('db.php');
+
+$db 		= new db();
+$sql 		= "SELECT * FROM usuario where usuario = '".$_REQUEST['usuario']."' AND password = '".$_REQUEST['password']."' AND activo = '1';";
+$response = $db->Select($sql);
+//$response = $user_data;
+header("Content-Type: application/json",true);
+echo json_encode(utf8_converter($response));
+
+function utf8_converter($array)
+{
+    array_walk_recursive($array, function(&$item, $key){
+        if(!mb_detect_encoding($item, 'utf-8', true)){
+                $item = utf8_encode($item);
+        }
+    });
+    return $array;
+}
+
+?>
