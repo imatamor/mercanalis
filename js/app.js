96dfd1855db134ff7f52e7c0b8df73be410dc3fa
Isaac Matamoros <isaacmg4@hotmail.com>
2019-01-14 01:04:48 -0500

    form image base64

--- js/app.js
+++ js/app.js
@@ -209,23 +209,18 @@ function saveElector()
   var numero_contrato         = $$('input[type=text][name=numero_contrato]').val();
   var image;
   toDataUrl(uploadimgdata, function(myBase64) {
-      console.log('myBase64');
-      console.log(myBase64); // myBase64 is the base64 string
-      console.log('image in');
+      //console.log(myBase64); // myBase64 is the base64 string
       image               = myBase64;
-      console.log(image);
+      var elector         = {'usuario':encuestador,'nombre': nombre,'apellido': apellido,'cedula': cedula,'fecha_nacimiento': fecha_nacimiento,'nombre_carnet': nombre_carnet,'nombre_familia': nombre_familia,'ciudad': ciudad,'canton': canton,'parroquia': parroquia,'barrio': barrio,'sector': sector,'direccion': direccion,'estado_civil': estado_civil,'numero_hijos': numero_hijos,'tiene_discapacidad': tiene_discapacidad,'discapacidad': discapacidad,'ocupacion': ocupacion,'profesion': profesion,'nivel_escolaridad': nivel_escolaridad,'capacitacion_deseada': capacitacion_deseada,'tiene_bono_gobierno': tiene_bono_gobierno,'tiene_bono_municipio': tiene_bono_municipio,'telefono_convencional': telefono_convencional,'telefono_celular': telefono_celular,'telefono_compania': telefono_compania,'tiene_whatsapp': tiene_whatsapp,'whatsapp': whatsapp,'tiene_facebook': tiene_facebook,'facebook': facebook,'tiene_instagram': tiene_instagram,'instagram': instagram,'tiene_twitter': tiene_twitter,'twitter': twitter,'correo_electronico': correo_electronico,'tiene_casa_propia': tiene_casa_propia,'tiene_vehiculo': tiene_vehiculo,'placa': placa,'seguro_medico': seguro_medico,'credito_agricola': credito_agricola,'otros': otros,'numero_contrato': numero_contrato,'image': image};
+      console.log(elector);
+      if(validateForm())
+      {
+        var directorioTmp = app.params.template7Data['directorio'];
+        directorioTmp.push(elector);
+        app.params.template7Data['directorio'] = directorioTmp;
+        updateStorage();
+      }
   });
-  console.log('image out');
-  console.log(image);
-  var elector                 = {'usuario':encuestador,'nombre': nombre,'apellido': apellido,'cedula': cedula,'fecha_nacimiento': fecha_nacimiento,'nombre_carnet': nombre_carnet,'nombre_familia': nombre_familia,'ciudad': ciudad,'canton': canton,'parroquia': parroquia,'barrio': barrio,'sector': sector,'direccion': direccion,'estado_civil': estado_civil,'numero_hijos': numero_hijos,'tiene_discapacidad': tiene_discapacidad,'discapacidad': discapacidad,'ocupacion': ocupacion,'profesion': profesion,'nivel_escolaridad': nivel_escolaridad,'capacitacion_deseada': capacitacion_deseada,'tiene_bono_gobierno': tiene_bono_gobierno,'tiene_bono_municipio': tiene_bono_municipio,'telefono_convencional': telefono_convencional,'telefono_celular': telefono_celular,'telefono_compania': telefono_compania,'tiene_whatsapp': tiene_whatsapp,'whatsapp': whatsapp,'tiene_facebook': tiene_facebook,'facebook': facebook,'tiene_instagram': tiene_instagram,'instagram': instagram,'tiene_twitter': tiene_twitter,'twitter': twitter,'correo_electronico': correo_electronico,'tiene_casa_propia': tiene_casa_propia,'tiene_vehiculo': tiene_vehiculo,'placa': placa,'seguro_medico': seguro_medico,'credito_agricola': credito_agricola,'otros': otros,'numero_contrato': numero_contrato,'image': image};
-  console.log(elector);
-  if(validateForm())
-  {
-    var directorioTmp = app.params.template7Data['directorio'];
-    directorioTmp.push(elector);
-    app.params.template7Data['directorio'] = directorioTmp;
-    updateStorage();
-  }
 }
 /*----------------------------------------------------------------------------------------------------------------------
 / Name: setFormPage
